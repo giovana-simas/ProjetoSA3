c76132c36e6941cb237f4be7b56be804394974fd
koohako <arturguimaraesdefreitas@gmail.com>
2020-10-20 19:34:07 -0200

    primeiro commit

---
+++ S.A/src/test/java/com/sa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
