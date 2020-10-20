e782fc4b07d999d0b0b826566db53767858219bd
koohako <arturguimaraesdefreitas@gmail.com>
2020-10-20 19:42:46 -0200

    projeto sem security mas com o index pronto

---
+++ S.A/src/main/java/com/sa/controller/indexController.java
@@ -0,0 +1,16 @@
+package com.sa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	
+}
