5e6bd8a3ecd3ad881b05c609c4856bdf1355f266
koohako <arturguimaraesdefreitas@gmail.com>
2021-03-10 17:45:19 -0300

    listagem de Usuarios na tela de chat

--- S.A/src/main/java/com/sa/controller/ChatController.java
+++ S.A/src/main/java/com/sa/controller/ChatController.java
@@ -1,13 +1,22 @@
 package com.sa.controller;
 
+import com.sa.model.Usuario;
+import com.sa.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
+
+	@Autowired
+	UsuarioRepository usuarioRepository;
 	
 	@GetMapping("/chat")
-	public String chat() {
+	public String chat(Model model) {
+		model.addAttribute("usuarios",usuarioRepository.findAll());
+
 		return "/chat/chat";
 	}
 	
